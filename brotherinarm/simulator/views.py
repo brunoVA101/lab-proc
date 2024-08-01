5c8e2d6357d86c216b2011e14751c6c67495e820
brunoVA101 <brunovalcantara@hotmail.com>
2024-08-01 19:25:03 -0300

    Use fastAPI in project

--- brotherinarm/simulator/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-from simulator.modules import codehandling
-
-def index(request):
-    context = {'code': codehandling.parse_lines()}
-    return render(request, "home.html", context=context)
