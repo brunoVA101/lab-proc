74fb82749854d7a84800d45b6655234550b27f64
brunoVA101 <brunovalcantara@hotmail.com>
2024-08-13 15:43:01 -0300

    implement compile endpoint

--- brother-in-arm/backend/emulator/runtime.py
+++ brother-in-arm/backend/emulator/runtime.py
@@ -8,9 +8,16 @@ class GdbRuntime:
         self.response = self.gdb.write('-file-exec-and-symbols /usr/bin/arm-none-eabi-gdb')
 
     def compile(self, code):
+        print("ok")
         with open('temp.s', 'w') as f:
             f.write(code)
         subprocess.run(['arm-none-eabi-as', '-o', 'temp.o', 'temp.s'])
         subprocess.run(['arm-none-eabi-ld', '-o', 'temp.elf', 'temp.o'])
         self.gdb.write('-file-exec-and-symbols temp.elf')
-        self.gdb.write('break _start')
+        self.gdb.write('-break-insert _start')
+        return self.gdb.write('-exec-run')
+    def go_next(self):
+        response = self.gdb.write('-exec-next')
+        return response
+    def get_state(self):
+        return self.gdb.get_gdb_response(timeout_sec=1)
--- brother-in-arm/backend/main.py
+++ brother-in-arm/backend/main.py
@@ -1,26 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
 from models.compile_request import CompileRequest
 from emulator.runtime import GdbRuntime
 
 app = FastAPI()
+runtime = GdbRuntime()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.post("/run/compile/")
-async def compile(request: CompileRequest):
-    GdbRuntime.compile(request.code)
-    return {"message": len(request.code)}
+async def compile(code: Annotated[str, Form()]):
+    return runtime.compile(code)
 
 @app.get("/run/next/")
 async def go_next():
-    return {"message": None}
+    return runtime.go_next()
 
 @app.get("/run/back/")
 async def go_back():
     return {"message": None}
 
 @app.get("/status/registers/")
-async def compile():
+async def status_registers():
     return {"message": None}
+
+@app.get("/status/")
+async def status():
+    return runtime.get_state()
