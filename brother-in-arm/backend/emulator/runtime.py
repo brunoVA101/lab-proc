f3bb7a98a50a91ceba409682421b8f62a9776070
brunoVA101 <brunovalcantara@hotmail.com>
2024-08-14 18:08:04 -0300

    fix compilation

--- brother-in-arm/backend/emulator/runtime.py
+++ brother-in-arm/backend/emulator/runtime.py
@@ -7,17 +7,18 @@ class GdbRuntime:
         # Envia um comando para iniciar o GDB com o binário especificado
         self.response = self.gdb.write('-file-exec-and-symbols /usr/bin/arm-none-eabi-gdb')
 
-    def compile(self, code):
+    def compile(self, code: str):
         print("ok")
         with open('temp.s', 'w') as f:
             f.write(code)
-        subprocess.run(['arm-none-eabi-as', '-o', 'temp.o', 'temp.s'])
-        subprocess.run(['arm-none-eabi-ld', '-o', 'temp.elf', 'temp.o'])
+        subprocess.run(['arm-none-eabi-as', '-o', 'temp.o', 'temp.s', '-g'])
+        subprocess.run(['arm-none-eabi-ld', '-T', 'linker.ld', '-o', 'temp.elf', 'temp.o', '-M=temp.map', '-L/usr/lib/gcc/arm-none-eabi/10.3.1/', '-lgcc'])
+        qemu_process = subprocess.Popen(['qemu-system-arm', '-s', '-M', 'virt', '-kernel', 'temp.elf'])
         self.gdb.write('-file-exec-and-symbols temp.elf')
-        self.gdb.write('-break-insert _start')
+        self.gdb.write('-break-insert main')
         return self.gdb.write('-exec-run')
     def go_next(self):
-        response = self.gdb.write('-exec-next')
+        response = self.gdb.write('next')
         return response
     def get_state(self):
         return self.gdb.get_gdb_response(timeout_sec=1)
