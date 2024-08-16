d2778d030c60bc56cf4e64a7ba645f25b1c95982
Matheus-Nakamura <matheusjunjinakamura@usp.br>
2024-08-16 14:36:59 -0300

    implemented basic endpoint call

--- brother-in-arm/backend/main.py
+++ brother-in-arm/backend/main.py
@@ -3,7 +3,22 @@ from typing import Annotated
 from models.compile_request import CompileRequest
 from emulator.runtime import GdbRuntime
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 runtime = GdbRuntime()
 
 @app.get("/")
--- brother-in-arm/frontend/labproc-front/src/app/home/home.component.ts
+++ brother-in-arm/frontend/labproc-front/src/app/home/home.component.ts
@@ -2,11 +2,33 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { ApiService } from './api.service';
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [MatIconModule, MatButtonModule, MatInputModule],
   templateUrl: './home.component.html',
+  
 })
-export class HomeComponent {}
+export class HomeComponent {
+  nextresponse: string | undefined;
+  constructor(private apiService: ApiService){ }
+  fetchText() : void {
+    this.apiService.getTextResponse().subscribe(
+      {
+        next: (data) => {
+        this.nextresponse = data;
+        },
+        error: (error) =>{
+          console.log(error)
+        },
+        complete: () =>{
+          console.log('complete')
+        }
+    }
+    );
+  }
+}
