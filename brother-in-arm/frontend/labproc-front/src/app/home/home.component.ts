15bd624aee326c117c174e8036754f83e68ad530
Matheus-Nakamura <matheusjunjinakamura@usp.br>
2024-08-18 15:06:11 -0300

    feat: set mem view in frontend

--- brother-in-arm/frontend/labproc-front/src/app/home/home.component.ts
+++ brother-in-arm/frontend/labproc-front/src/app/home/home.component.ts
@@ -38,6 +38,7 @@ export class HomeComponent {
   inst: string = 'Aguardando próxima instrução';
   cpsr: string = '00000000000000000000000000000000';
   positions: string = '';
+  
   analysisInst(){
     var parsed = this.inst.replace(/,/g, '').split(' ');
     //parsed.forEach(word => {
@@ -46,6 +47,7 @@ export class HomeComponent {
     var firstarg = parsed[1];
     var secondarg = parsed[2];
     var thirdarg = parsed[3];
+    var translation = '';
     for(let i = 12; i >= 0; i--){
       const regex = new RegExp(`r${i}`, 'gi');
       if(regex.test(parsed[1])){
@@ -58,31 +60,118 @@ export class HomeComponent {
         thirdarg = parsed[3].replace(regex, this.labelarray[i]);
       }
     }
-    // var firstarg = this.labelarray[0]
-    switch(parsed[0]){
+    
+    switch(parsed[0].toLocaleUpperCase().slice(0, 3)){
       case "BL":
-        return "Saltar para subrotina " + parsed[1]
+        translation += "Saltar para subrotina " + firstarg;
+        break;
+      case "B":
       case "BEQ":
-        return "Saltar para subrotina " + parsed[1] + " se resultado for zero"
+      case "BMI":
+      case "BPL":
+        translation += "Saltar para " + firstarg;
+        break;
       case "MOV":
-        return "Armazenar em " + firstarg + " a constante " + parsed[2]
+        translation += "Armazenar em " + firstarg + " a constante " + secondarg;
+        break;
       case "SUB":
-      case "SUBS":
-        return "Armazenar em " + firstarg + " " + secondarg + " menos " + thirdarg;
+        translation += "Armazenar em " + firstarg + " " + secondarg + " menos " + thirdarg;
+        break;
       case "ADD":
-      case "ADDS":
-        return "Armazenar em " + firstarg + " " + secondarg + " mais " + thirdarg;
+        translation += "Armazenar em " + firstarg + " " + secondarg + " mais " + thirdarg;
+        break;
       case "STR":
-        return "A posição de memória " + secondarg +"+"+ thirdarg + " receberá o valor de " + firstarg;
+        translation += "A posição de memória " + secondarg +"+"+ thirdarg + " receberá o valor de " + firstarg;
+        break;
       case "EOR":
-      case "EORS":
-        return "Armazenar em " + firstarg + " " + secondarg + " XOR " + thirdarg;
+        translation += "Armazenar em " + firstarg + " " + secondarg + " XOR " + thirdarg;
+        break;
       case "BX":
-        return "Retornar de subrotina: endereço " + parsed[1]
+        translation += "Retornar de subrotina: endereço " + firstarg;
+        break;
+      case "LDR":
+        if(typeof parsed[3] !== 'undefined'){
+          translation += "Carregar em " + firstarg + ", " + secondarg +"+ " + thirdarg
+        }
+        translation += "Carregar em " + firstarg + ", " + secondarg
+        break;
+      case "AND":
+          translation += "Armazenar em " + firstarg + " " + secondarg + " E " + thirdarg;
+          break;
+      case "ORR":
+        translation += "Armazenar em " + firstarg + " " + secondarg + " OU " + thirdarg;
+        break;
       default:
-        return "..."
+        translation += "..."
+        break;
     }
-    return parsed[0] + "Hello world"
+    if(typeof parsed[3] !== 'undefined' && typeof parsed[4] !== 'undefined' ){
+      switch(parsed[3].toLocaleUpperCase()){
+        case "LSL":
+          translation += " com " + parsed[4].slice(1) + " shift(s) à esquerda.";
+          break;
+        case "LSR":
+          translation += " com " + parsed[4].slice(1) + " shift(s) à direita.";
+          break;
+        case "ASR":
+          translation += " com " + parsed[4].slice(1) + " shift(s) à direita(sinal estendido).";
+          break;
+        case "ROR":
+          translation += " com " + parsed[4].slice(1) + " rotação(ões) à direita.";
+          break;
+        default:
+          break;
+      }
+    }
+
+    if(typeof parsed[4] !== 'undefined' && typeof parsed[5] !== 'undefined' ){
+      switch(parsed[4].toLocaleUpperCase()){
+        case "LSL":
+          translation += " com " + parsed[5].slice(1) + " shift(s) à esquerda.";
+          break;
+        case "LSR":
+          translation += " com " + parsed[5].slice(1) + " shift(s) à direita.";
+          break;
+        case "ASR":
+          translation += " com " + parsed[5].slice(1) + " shift(s) à direita(sinal estendido).";
+          break;
+        case "ROR":
+          translation += " com " + parsed[5].slice(1) + " rotação(ões) à direita.";
+          break;
+        default:
+          break;
+      }
+    }
+    
+    switch(parsed[0].toLocaleUpperCase().slice(-2)){
+      case "CS":
+        translation += " com carry"
+        break;
+      case "EQ":
+        translation += " se Z=1(igualdade)"
+        break;
+      case "NE":
+        translation += " se Z=0(desigualdade)"
+        break;
+      case "PL":
+        translation += " se N=0(resultado 0 ou +)"
+        break;
+      case "VS":
+        translation += " se overflow"
+        break;
+      case "VC":
+        translation += " se não overflow"
+        break;
+      case "MI":
+        translation += " se N=1(resultado -)"
+        break;
+      case "PL":
+        translation += " se N=0(resultado 0 ou +)"
+        break;
+      default:
+        break;
+    }
+    return translation;
   }
   sendCompile() {
     this.apiService.sendCompile(this.code).subscribe({
